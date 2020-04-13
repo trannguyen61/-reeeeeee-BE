1e4c79992cd365d94e97494f1082e6a9019a8e4c
Tran Nguyen <trannguyen61st@gmail.com>
2020-04-13 16:43:30 +0700

    changes in env name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const db = require('./db/db')
 
 const app = express()
-const port = process.env.SERVER_PORT || 3000
+const port = process.env.PORT || 3000
 
 const userRoutes = require('./routers/user')
 const searchRoutes = require('./routers/search')
